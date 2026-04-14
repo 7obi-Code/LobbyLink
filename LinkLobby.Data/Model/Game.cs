44727fcc742f1342e53dc7cddc64e3f06e508195
Quitoooo <noahjj@outlook.dk>
2026-04-14 13:27:41 +0200

    ItemDefinition.cs udkast (bruges som temp)

--- LinkLobby.Data/Model/Game.cs
+++ LinkLobby.Data/Model/Game.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LobbyLink.DataAccess.Model
+namespace LobbyLink.DataAccess.Model;
+public class Game
 {
-    internal class Game
-    {
-    }
 }
---
+++ LinkLobby.Data/Model/ItemDefinition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LobbyLink.DataAccess.Model;
+public class ItemDefinition
+{
+    public string ItemName { get; set; }
+    public string ItemImageUrl { get; set; }
+    public string ItemDescription { get; set; }
+    public Game GameDefinition { get; set; }
+    public List<ItemPropertyLine> ItemPropertyLines { get; set; }
+
+    public ItemDefinition(string itemName, string itemImageUrl, string itemDescription, Game gameDefinition, List<ItemPropertyLine> itemPropertyLines)
+    {
+        ItemName = itemName;
+        ItemImageUrl = itemImageUrl;
+        ItemDescription = itemDescription;
+        GameDefinition = gameDefinition;
+        ItemPropertyLines = itemPropertyLines;
+    }
+}
