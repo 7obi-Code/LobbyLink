aa8e37c0ab85d81e2ff74a29dc311a8842485195
Quitoooo <noahjj@outlook.dk>
2026-04-15 11:03:02 +0200

    Model: ItemPropertyLine, ItemPropertyValue

--- LobbyLink.DataAccess/Model/ItemPropertyLine.cs
+++ LobbyLink.DataAccess/Model/ItemPropertyLine.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LobbyLink.DataAccess.Model
+namespace LobbyLink.DataAccess.Model;
+public class ItemPropertyLine
 {
-    internal class ItemPropertyLine
-    {
-    }
+    public int Id { get; set; }
+    public ItemDefinition ItemDefiniton { get; set; }
+    public ItemProperty ItemProperty { get; set; }
+    public ItemPropertyValue ItemPropertyValue { get; set; }
 }
