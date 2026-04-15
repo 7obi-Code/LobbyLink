8eeae1cf4535233befb595a4862f71ac578eff7c
Laurakjb <1086719@ucn.dk>
2026-04-15 11:12:43 +0200

    ItemInstance og ItemInstanceProp

--- LobbyLink.DataAccess/Model/ItemInstanceProperty.cs
+++ LobbyLink.DataAccess/Model/ItemInstanceProperty.cs
@@ -5,5 +5,15 @@ using System.Text;
 namespace LobbyLink.DataAccess.Model;
 public class ItemInstanceProperty
 {
+ 
+
+    public string ItemInstancePropertyName { get; set; }    
+    public string ItemInstancePropertyValue { get; set; }
+
+    public ItemInstanceProperty(string itemInstancePropertyName, string itemInstancePropertyValue)
+    {
+        ItemInstancePropertyName = itemInstancePropertyName;
+        ItemInstancePropertyValue = itemInstancePropertyValue;
+    }
 
 }
