f2e4c088bf17d23b2c69faa366374c0e32ef5172
Quitoooo <noahjj@outlook.dk>
2026-04-16 08:19:05 +0200

    IFItemInstanceDao og ItemInsatnceDao

--- LobbyLink.DataAccess/Interfaces/IFItemInstanceDao.cs
+++ LobbyLink.DataAccess/Interfaces/IFItemInstanceDao.cs
@@ -8,11 +8,10 @@ namespace LobbyLink.DataAccess.Interfaces
 {
     public interface IFItemInstanceDao
     {
-        ItemInstance GetItemInstanceById(int id);
+        ItemInstance? GetItemInstanceById(int id);
         IEnumerable<ItemInstance> GetAllItemInstance();
         int InsertItemInstance(ItemInstance itemInstance);
         bool UpdateItemInstance(ItemInstance itemInstance);
-        bool DeleteItemInstance(ItemInstance itemInstance);
         bool DeleteItemInstanceById(int id);
     }
 }
