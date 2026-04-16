d91b06c2ed091b93b3db32302bff0b8216ab207e
7obi-Code <tobiaswalsted@gmail.com>
2026-04-16 10:38:40 +0200

    Add TestSettings, new item test; remove IFItemDao
    Centralize test DB connection and add test scaffolding: adds TestSettings.cs (CONNECTION_STRING) and a new ItemHasItemDefinition test class with setup/teardown and cleanup lists. Updates ItemInstanceOwnershipTest to use TestSettings.CONNECTION_STRING instead of a hardcoded string. Removes an unused internal interface file IFItemDao.cs.

---
+++ LobbyLink.Test/DaoTests/TestSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LobbyLink.Test.DaoTests
+{
+    internal static class TestSettings
+    {
+        public const string CONNECTION_STRING = "Data Source=hildur.ucn.dk;Initial Catalog=DMA-CSD-V252_10666018;User ID=DMA-CSD-V252_10666018;Password=Password1!;Trust Server Certificate=True";
+    }
+}
