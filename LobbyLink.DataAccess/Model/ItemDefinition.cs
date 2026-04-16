560e39472f7fae4548d4db59507ced331d1657db
q2kkK <jonasdahllarsen@live.dk>
2026-04-16 10:37:27 +0200

    Oprettelse af ItemDefinitionDao.cs
    Samt tilføjelse af GameID_FK. Er dog lidt usikker på om det er den rigtige måde at gøre det på.

--- LobbyLink.DataAccess/Model/ItemDefinition.cs
+++ LobbyLink.DataAccess/Model/ItemDefinition.cs
@@ -10,15 +10,18 @@ public class ItemDefinition
     public string ItemImageUrl { get; set; }
     public string ItemDescription { get; set; }
     public Game GameDefinition { get; set; }
+    public int GameId_FK { get; set; }
     public List<ItemPropertyLine> ItemPropertyLines { get; set; }
 
-    public ItemDefinition(int itemDefinitionId, string itemName, string itemImageUrl, string itemDescription, Game gameDefinition, List<ItemPropertyLine> itemPropertyLines)
+    public ItemDefinition(int itemDefinitionId, string itemName, string itemImageUrl, string itemDescription, int gameId_FK, Game gameDefinition, List<ItemPropertyLine> itemPropertyLines)
     {
         ItemDefinitionId = itemDefinitionId;
         ItemName = itemName;
         ItemImageUrl = itemImageUrl;
         ItemDescription = itemDescription;
+        GameId_FK = gameId_FK;
         GameDefinition = gameDefinition;
         ItemPropertyLines = itemPropertyLines;
+
     }
 }
