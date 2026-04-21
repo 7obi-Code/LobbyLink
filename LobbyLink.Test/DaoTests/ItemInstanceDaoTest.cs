d20a6d7a8f1e1ff5db284727f9c3fbb07ed77e1b
Quitoooo <noahjj@outlook.dk>
2026-04-21 09:04:11 +0200

    Oprettelse af Test skelet til TDD

---
+++ LobbyLink.Test/DaoTests/ItemInstanceDaoTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LobbyLink.Test.DaoTests;
+
+public class ItemInstanceDaoTest
+{
+    [Test]
+    public void GetItemInstancesByAccount_ShouldReturnItemInstances_WhenAccountHasItemInstances()
+    {
+
+    }
+
+    public void GetItemInstanceDefinitionNameByItemInstance_ShouldReturnDefinitionName()
+    {
+
+    }
+}
---
+++ LobbyLink.Test/DaoTests/ListingsDaoTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LobbyLink.Test.DaoTests;
+public class ListingsDaoTest
+{
+    [Test]
+    public void GetAllActiveListings_ShouldReturnListings_WhenListingsIsAvailable()
+    {
+
+    }
+
+    [Test]
+    public void GetAllActiveListings_ShouldIncludePrice_ForEachListing()
+    {
+
+    }
+
+    [Test]
+    public void GetActiveListings_ShouldIncludeItemInstance_ForEachListing()
+    {
+
+    }
+
+    [Test]
+    public void GetActiveListings_ShouldIncludeAccount_ForEachListing()
+    {
+
+    }
+}
